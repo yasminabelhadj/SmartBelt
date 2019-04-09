8a982e1b6b8ebd060a853267e309e2c3b26efe0f
4KINGZ <hsan.dellel@supcom.tn>
2019-04-09 14:25:52 +0100

    added activities

---
+++ app/src/main/java/com/example/hp/ournetwork/BackView.java
@@ -0,0 +1,13 @@
+package com.example.hp.ournetwork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BackView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_back_view);
+    }
+}
--- app/src/main/java/com/example/hp/ournetwork/FrontView.java
+++ app/src/main/java/com/example/hp/ournetwork/FrontView.java
@@ -1,13 +1,49 @@
 package com.example.hp.ournetwork;
 
+import android.content.Intent;
+import android.hardware.Sensor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FrontView extends AppCompatActivity {
+    private Button left, right, back;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_events);
+        setContentView(R.layout.activity_front_view);
+        left = (Button) findViewById(R.id.left);
+        right = (Button) findViewById(R.id.right);
+        back = (Button) findViewById(R.id.back);
+
+        //onClick();
+
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.left:
+                Intent ileft = new Intent(FrontView.this, BackView.class);
+                startActivity(ileft);
+
+                break;
+            case R.id.right:
+
+                //layout.removeAllViews();
+                //openChart();
+
+                // show data in chart
+                break;
+
+            case R.id.back:
+
+
+            default:
+                break;
+        }
+
     }
 }
